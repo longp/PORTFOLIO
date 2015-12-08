bac82c7e21f33f7923b4cb66eba3fd0dbe09d652
long phan <longphan@longs-MacBook-Pro.local>
2015-12-07 21:43:48 -0500

    removed js scroll fx

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,31 @@
 $(document).ready(function(){
 $(".jumbotron").hide();
 
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0); 
+});
 
-// scrolling to section on click btn
-$("a[href='#top']").click(function() { 
+
+// // scrolling to section on click btn
+// $("a[href='#top']").click(function() { 
   
-  $("html, body").animate({ scrollTop: 0 }, "fast"); return false; });
+//   $("html, body").animate({ scrollTop: 0 }, "fast"); return false; });
 
 
-$('a[href$="portfolio"]').click(function() {
-     $('html, body').animate({
-         scrollTop: $("#portfolio").offset().top   }, 500);
- });
+// $('a[href$="portfolio"]').click(function() {
+//      $('html, body').animate({
+//          scrollTop: $("#portfolio").offset().top   }, 500);
+//  });
 
-$('a[href$="blog"]').click(function() {
-     $('html, body').animate({
-         scrollTop: $("#blog").offset().top   }, 500);
- });
+// $('a[href$="blog"]').click(function() {
+//      $('html, body').animate({
+//          scrollTop: $("#blog").offset().top   }, 500);
+//  });
 
-$('a[href$="about"]').click(function() {
-     $('html, body').animate({
-         scrollTop: $("#about").offset().top   }, 500);
- });
+// $('a[href$="about"]').click(function() {
+//      $('html, body').animate({
+//          scrollTop: $("#about").offset().top   }, 500);
+//  });
 
 
 
