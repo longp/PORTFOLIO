3190b2f5d1634f7a77701a3b24cf98a1bb8571c7
long phan <longphan@longs-MacBook-Pro.local>
2015-12-07 22:28:30 -0500

    added profile pic

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,51 +1,54 @@
 $(document).ready(function(){
-$(".jumbotron").hide();
+// $(".jumbotron").hide();
+//  $(".begin") .css("z-index","200")
 
-$(window).on('beforeunload', function() {
-    $(window).scrollTop(0); 
-});
+// $(window).on('beforeunload', function() {
+//     $(window).scrollTop(0); 
+// });
 
 
-// // scrolling to section on click btn
-// $("a[href='#top']").click(function() { 
+// scrolling to section on click btn
+$("a[href='#top']").click(function() { 
   
-//   $("html, body").animate({ scrollTop: 0 }, "fast"); return false; });
+  $("html, body").animate({ scrollTop: 0 }, "fast"); return false; });
 
 
-// $('a[href$="portfolio"]').click(function() {
-//      $('html, body').animate({
-//          scrollTop: $("#portfolio").offset().top   }, 500);
-//  });
+$('a[href$="portfolio"]').click(function() {
+     $('html, body').animate({
+         scrollTop: $("#portfolio").offset().top   }, 500);
+ });
 
-// $('a[href$="blog"]').click(function() {
-//      $('html, body').animate({
-//          scrollTop: $("#blog").offset().top   }, 500);
-//  });
+$('a[href$="blog"]').click(function() {
+     $('html, body').animate({
+         scrollTop: $("#blog").offset().top   }, 500);
+ });
 
-// $('a[href$="about"]').click(function() {
-//      $('html, body').animate({
-//          scrollTop: $("#about").offset().top   }, 500);
-//  });
+$('a[href$="about"]').click(function() {
+     $('html, body').animate({
+         scrollTop: $("#about").offset().top   }, 500);
+ });
 
 
 
-$(window).scroll(function () {
-    hideVid();
-    if($(document).scrollTop() == 0) showVid();
-  });
+// $(window).scroll(function () {
+//     hideVid();
+//     if($(document).scrollTop() == 0) showVid();
+//   });
 });
 
 
 
 
-// video FX
-function hideVid() {
-$("#bgvid").css("z-index","-100");
-$(".jumbotron").show();
+// // video FX
+// function hideVid() {
+// $("#bgvid").css("z-index","-100");
+// $(".jumbotron").show();
 
 
-} 
-function showVid() {
-  $("#bgvid").css("z-index","100"); 
-}
+
+// } 
+// function showVid() {
+//   $("#bgvid").css("z-index","100");
+//   $(".begin") .css("z-index","200")
+// }
 
