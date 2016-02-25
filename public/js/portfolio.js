b00feb309f7681e6581a2781c8291e8e5851cf6d
Long Phan <lphan1989@gmail.com>
2016-02-25 00:59:15 -0500

    added fading background to body

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-// $(".jumbotron").hide();
-//  $(".begin") .css("z-index","200")
+$(window).load(function() {
+$('body').addClass('bg1');
+});
 
-// $(window).on('beforeunload', function() {
-//     $(window).scrollTop(0); 
-// });
+$(document).ready(function(){
 
 
 // scrolling to section on click btn
@@ -29,26 +27,67 @@ $('a[href$="about"]').click(function() {
  });
 
 
+changeBackground();
 
-// $(window).scroll(function () {
-//     hideVid();
-//     if($(document).scrollTop() == 0) showVid();
-//   });
 });
 
 
+// switches background images of body
+function changeBackground () {
+	setInterval(function() {
+		var $body = $('body');
+		if ($body.hasClass("bg1")) {
+			switchBackground("bg1", "bg2");
+		}
+		else if ($body.hasClass("bg2")) {
+			switchBackground("bg2", "bg3");
+		}
+		else if ($body.hasClass("bg3")) {
+			switchBackground("bg3", "bg4");
+		}
+		else if ($body.hasClass("bg4")) {
+			switchBackground("bg4", "bg5");
+		}
+		else if ($body.hasClass("bg5")) {
+			switchBackground("bg5", "bg6");
+		}
+		else if ($body.hasClass("bg6")) {
+			switchBackground("bg6", "bg7");
+		}
+		else if ($body.hasClass("bg7")) {
+			switchBackground("bg7", "bg8");
+		}
+		else if ($body.hasClass("bg8")) {
+			switchBackground("bg8", "bg1");
+		}
+	}, 5000);
+}
 
 
-// // video FX
-// function hideVid() {
-// $("#bgvid").css("z-index","-100");
-// $(".jumbotron").show();
+function switchBackground (firstBG, secondBG) {
+	var $body = $('body');
+	$body.removeClass(firstBG);
+  $body.addClass(secondBG);
+    
+}
 
+// trying to make promise
+function myfunction () {
+	promise = first().then(second).then(third);	
+}
 
-
-// } 
-// function showVid() {
-//   $("#bgvid").css("z-index","100");
-//   $(".begin") .css("z-index","200")
-// }
-
+function first () {
+	d = new $.Deferred();
+	switchBackground("bg1", "bg2");
+	return d.promise()
+}
+function second () {
+	d = new $.Deferred();
+	switchBackground("bg2", "bg3");
+	return d.promise()
+}
+function third () {
+	d = new $.Deferred();
+	switchBackground("bg3", "bg1");
+	return d.promise()
+}
